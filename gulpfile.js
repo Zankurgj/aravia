8b970f25f8525cac33b3d0eae4e78542aa49952f
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2021-01-29 11:59:49 +0300

    popup + fancy

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
     'node_modules/slick-carousel/slick/slick.min.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     // 'node_modules/masonry-layout/dist/masonry.pkgd.js',
     // 'node_modules/imagesloaded/imagesloaded.pkgd.js',
     // 'node_modules/swiper/swiper-bundle.js',
