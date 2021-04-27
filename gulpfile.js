fe3c54c8b8050edea7ce8255d0a7dcc78ffe60a3
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2021-04-27 23:36:14 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-let preprocessor = 'scss'; // Preprocessor (sass, scss)
-let fileswatch = 'html,htm,txt,json,md'; // List of files extensions for watching & hard reload (comma separated)
-let imageswatch = 'jpg,jpeg,png,webp,svg,woff2'; // List of files extensions for watching & hard reload (comma separated)
+let preprocessor = "scss"; // Preprocessor (sass, scss)
+let fileswatch = "html,htm,txt,json,md"; // List of files extensions for watching & hard reload (comma separated)
+let imageswatch = "jpg,jpeg,png,webp,svg,woff2"; // List of files extensions for watching & hard reload (comma separated)
 
-const { src, dest, parallel, series, watch } = require('gulp');
-const sass = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const newer = require('gulp-newer');
-const rsync = require('gulp-rsync');
-const del = require('del');
-const gulp = require('gulp');
+const { src, dest, parallel, series, watch } = require("gulp");
+const sass = require("gulp-sass");
+const cleancss = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify-es").default;
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const rsync = require("gulp-rsync");
+const del = require("del");
+const gulp = require("gulp");
 
 // Local Server
 
 function browsersync() {
   browserSync.init({
-    server: { baseDir: 'app' },
+    server: { baseDir: "app" },
     notify: false,
     // online: false, // Work offline without internet connection
   });
@@ -28,14 +28,14 @@ function browsersync() {
 // Custom Styles
 
 function styles() {
-  return src('app/sass/main.' + preprocessor + '')
+  return src("app/sass/main." + preprocessor + "")
     .pipe(sass())
-    .pipe(concat('app.min.css'))
+    .pipe(concat("app.min.css"))
     .pipe(
-      autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })
+      autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true })
     )
     .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
-    .pipe(dest('app/css'))
+    .pipe(dest("app/css"))
     .pipe(browserSync.stream());
 }
 
@@ -43,44 +43,44 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-    'node_modules/slick-carousel/slick/slick.min.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    "node_modules/jquery/dist/jquery.min.js", // npm vendor example (npm i --save-dev jquery)
+    "node_modules/slick-carousel/slick/slick.min.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
     // 'node_modules/masonry-layout/dist/masonry.pkgd.js',
     // 'node_modules/imagesloaded/imagesloaded.pkgd.js',
     // 'node_modules/swiper/swiper-bundle.js',
-    'node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
-    'app/js/app.js', // app.js. Always at the end
+    // 'node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
+    "app/js/app.js", // app.js. Always at the end
   ])
-    .pipe(concat('app.min.js'))
+    .pipe(concat("app.min.js"))
     .pipe(uglify()) // Minify JS (opt.)
-    .pipe(dest('app/js'))
+    .pipe(dest("app/js"))
     .pipe(browserSync.stream());
 }
 
 // Images
 
 function images() {
-  return src('app/images/src/**/*')
-    .pipe(newer('app/images/dest'))
+  return src("app/images/src/**/*")
+    .pipe(newer("app/images/dest"))
     .pipe(imagemin())
-    .pipe(dest('app/images/dest'));
+    .pipe(dest("app/images/dest"));
 }
 
 function cleanimg() {
-  return del('app/images/dest/**/*', { force: true });
+  return del("app/images/dest/**/*", { force: true });
 }
 
 // Deploy
 
 function deploy() {
-  return src('app/').pipe(
+  return src("app/").pipe(
     rsync({
-      root: 'app/',
-      hostname: 'username@yousite.com',
-      destination: 'yousite/public_html/',
+      root: "app/",
+      hostname: "username@yousite.com",
+      destination: "yousite/public_html/",
       // include: ['*.htaccess'], // Included files
-      exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
+      exclude: ["**/Thumbs.db", "**/*.DS_Store"], // Excluded files
       recursive: true,
       archive: true,
       silent: false,
@@ -91,10 +91,10 @@ function deploy() {
 // Watching
 
 function startwatch() {
-  watch('app/sass/**/*.' + preprocessor + '', parallel('styles'));
-  watch(['app/**/*.js', '!app/js/*.min.js'], parallel('scripts'));
-  watch(['app/**/*.{' + imageswatch + '}'], parallel('images'));
-  watch(['app/**/*.{' + fileswatch + '}']).on('change', browserSync.reload);
+  watch("app/sass/**/*." + preprocessor + "", parallel("styles"));
+  watch(["app/**/*.js", "!app/js/*.min.js"], parallel("scripts"));
+  watch(["app/**/*.{" + imageswatch + "}"], parallel("images"));
+  watch(["app/**/*.{" + fileswatch + "}"]).on("change", browserSync.reload);
 }
 
 exports.browsersync = browsersync;
